fca9dff1a92828a4ff1128d06b62a6898f831e9c
Bytes-mitesh <socialdevp786@gmail.com>
2023-03-22 23:20:15 +0530

    first frontend init

---
+++ build/precache-manifest.99a7bba7dc30b43b9a5fce79f8414a60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9c97b2fb92667b3b6c9",
+    "url": "/static/css/main.99e5c9f1.chunk.css"
+  },
+  {
+    "revision": "b9c97b2fb92667b3b6c9",
+    "url": "/static/js/main.dcb2c230.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c44af2f97180d850d7d6",
+    "url": "/static/js/2.bc4e413d.chunk.js"
+  },
+  {
+    "revision": "4290bc807f898ade8bc9a4296735ab10",
+    "url": "/static/media/newLogo.4290bc80.svg"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/static/media/shopping-bag.09e6f5e7.svg"
+  },
+  {
+    "revision": "e4550b77c6412eae691c2927158c98b6",
+    "url": "/index.html"
+  }
+];
---
+++ shopData/shop.data.js
@@ -0,0 +1,153 @@
+const SHOP_DATA = [{
+        id: 1,
+        title: 'Hats',
+        routeName: 'hats',
+        items: [{
+                id: 1,
+                name: 'Brown Brim',
+                imageUrl: "/images/shop-img/hats/brown-brim.png",
+                price: 25
+            },
+            {
+                id: 2,
+                name: 'Blue Beanie',
+                imageUrl: "/images/shop-img/hats/blue-beanie.png",
+                price: 18
+            },
+            {
+                id: 3,
+                name: 'Brown Cowboy',
+                imageUrl: "/images/shop-img/hats/brown-cowboy.png",
+                price: 35
+            },
+            {
+                id: 4,
+                name: 'Grey Brim',
+                imageUrl: "/images/shop-img/hats/grey-brim.png",
+                price: 25
+            },
+            {
+                id: 5,
+                name: 'Green Beanie',
+                imageUrl: "/images/shop-img/hats/green-beanie.png",
+                price: 18
+            },
+            {
+                id: 6,
+                name: 'Palm Tree Cap',
+                imageUrl: "/images/shop-img/hats/palm-tree-cap.png",
+                price: 14
+            },
+            {
+                id: 7,
+                name: 'Red Beanie',
+                imageUrl: "/images/shop-img/hats/red-beanie.png",
+                price: 18
+            },
+            {
+                id: 8,
+                name: 'Wolf Cap',
+                imageUrl: "/images/shop-img/hats/wolf-cap.png",
+                price: 14
+            },
+            {
+                id: 9,
+                name: 'Blue Snapback',
+                imageUrl: "/images/shop-img/hats/blue-snapback.png",
+                price: 16
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: 'Sneakers',
+        routeName: 'sneakers',
+        items: [{
+                id: 10,
+                name: 'Adidas NMD',
+                imageUrl: "/images/shop-img/sneakers/adidas-nmd.png",
+                price: 220
+            },
+            {
+                id: 11,
+                name: 'Adidas Yeezy',
+                imageUrl: "/images/shop-img/sneakers/yeezy.png",
+                price: 280
+            },
+            {
+                id: 12,
+                name: 'Black Converse',
+                imageUrl: "/images/shop-img/sneakers/black-converse.png",
+                price: 110
+            },
+            {
+                id: 13,
+                name: 'Nike White AirForce',
+                imageUrl: "/images/shop-img/sneakers/white-nike-high-tops.png",
+                price: 160
+            },
+            {
+                id: 14,
+                name: 'Nike Red High Tops',
+                imageUrl: "/images/shop-img/sneakers/nikes-red.png",
+                price: 160
+            },
+            {
+                id: 15,
+                name: 'Nike Brown High Tops',
+                imageUrl: "/images/shop-img/sneakers/nike-brown.png",
+                price: 160
+            },
+            {
+                id: 16,
+                name: 'Air Jordan Limited',
+                imageUrl: "/images/shop-img/sneakers/nike-funky.png",
+                price: 190
+            },
+            {
+                id: 17,
+                name: 'Timberlands',
+                imageUrl: "/images/shop-img/sneakers/timberlands.png",
+                price: 200
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: 'Jackets',
+        routeName: 'jackets',
+        items: [{
+                id: 18,
+                name: 'Black Jean Shearling',
+                imageUrl: "/images/shop-img/jackets/black-shearling.png",
+                price: 125
+            },
+            {
+                id: 19,
+                name: 'Blue Jean Jacket',
+                imageUrl: "/images/shop-img/jackets/blue-jean-jacket.png",
+                price: 90
+            },
+            {
+                id: 20,
+                name: 'Grey Jean Jacket',
+                imageUrl: "/images/shop-img/jackets/grey-jean-jacket.png",
+                price: 90
+            },
+            {
+                id: 21,
+                name: 'Brown Shearling',
+                imageUrl: "/images/shop-img/jackets/brown-shearling.png",
+                price: 165
+            },
+            {
+                id: 22,
+                name: 'Tan Trench',
+                imageUrl: "/images/shop-img/jackets/brown-trench.png",
+                price: 185
+            }
+        ]
+    }
+];
+
+export default SHOP_DATA;
